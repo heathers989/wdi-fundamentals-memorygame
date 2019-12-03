2559afe5b2c0a4bdc0f465d4afc7c838025bd65c
Heather Spoelstra <heatherspoelstra@Heathers-Air.home>
2019-12-02 19:25:03 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,27 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+//create array for cards
+const cards = ["queen", "queen", "king", "king"];
+//remove variables
+//create empty array for cards in play
+const cardsInPlay = [];
+
+//create a variable for first card flipped
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+}
+else {
+alert ("Sorry, try again.");
+}
+
